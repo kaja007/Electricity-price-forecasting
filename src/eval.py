4d4dd3fcfff216fef54620f2a5bc5ae508bbb393
kaja007 <53329458+kaja007@users.noreply.github.com>
2026-03-25 21:57:38 +0530

    Update eval.py

--- src/eval.py
+++ src/eval.py
@@ -1,6 +1,5 @@
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
-
 def evaluate_predictions(y_true, preds, name="Model"):
     mae = mean_absolute_error(y_true, preds)
     rmse = np.sqrt(mean_squared_error(y_true, preds))
